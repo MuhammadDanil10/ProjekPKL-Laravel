538da7394631fa3c80a4c9ea7f1e7e93c7c5c325
Muhammad Danil <mdanil180215@gmail.com>
2023-06-17 23:29:28 +0700

    Navbar - endPage

--- resources/views/layouts/mainNavbar.blade.php
+++ resources/views/layouts/mainNavbar.blade.php
@@ -8,6 +8,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+
   <title>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Fonts -->
@@ -28,60 +29,55 @@
 </head>
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color: #145DA0; height: 80px;">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="{{asset('assets/img/logovote.png')}}" width="50" height="50" class="d-inline-block align-top" alt="">
-        </a>
-        <a class="nav-link" href="https://smkn4pyk.sch.id/" style="color: aliceblue; font-weight:800; font-size:20px">E-Voting</a>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark p-3" style="background-color: #145DA0">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="{{route('welcome')}}">E-Voting</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav">
-            <!-- Add your left side navigation items here -->
-          </ul>
-
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ms-auto">
-            <!-- Authentication Links -->
-            <li class="nav-item dropdown"> 
-              <a class="nav-link" href="https://linktr.ee/kukicha" class="text-white">Help</a>
+    
+        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto ">
+            <li class="nav-item">
+              <a class="nav-link mx-2 active" aria-current="page" href="{{route('welcome')}}">Home</a>
             </li>
-            <li class="nav-item dropdown"> 
-              <a href="https://api.whatsapp.com/send?phone=6282237943100&text=Hola%20este%20es%20el%20mensaje" class="nav-link">Contact</a>
+            <li class="nav-item">
+              <a class="nav-link mx-2 active" href="https://api.whatsapp.com/send?phone=6282237943100&text=Hola%20este%20es%20el%20mensaje">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link mx-2 active" href="https://linktr.ee/kukicha">Help</a>
             </li>
             @guest
-              @if (Route::has('login'))
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                </li>
-              @endif
-            @else
-              <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                  {{ Auth::user()->name }}
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
-                    Dahboard
-                  </a>
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                     onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-                     </i>Logout
-                  </a>
-
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                  </form>
-                </div>
+            @if (Route::has('login'))
+              <li class="nav-item">
+                <a class="nav-link mx-2 active" href="{{ route('login') }}">{{ __('Login') }}</a>
               </li>
-            @endguest
+            @endif
+          @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle mx-2 active" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+              </a>
+  
+              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                  Dahboard
+                </a>
+                <a class="dropdown-item " href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">
+                   </i>Logout
+                </a>
+  
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
+              </div>
+            </li>
+          @endguest
+          </ul>
+          <ul class="navbar-nav ms-auto d-none d-lg-inline-flex">
+            {{-- right menu --}}
           </ul>
         </div>
       </div>
